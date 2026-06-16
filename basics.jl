968ccf6d10711ed51184503cd2df802d9ec6105b
jackson_jones <your@email.com>
2026-06-16 14:16:51 +0100

    Add Julia basics script

---
+++ basics.jl
@@ -0,0 +1,23 @@
+# Variables and maths
+x = 5
+y = 2.5
+println(x + y)
+
+# Strings
+name = "Jackson"
+println("Hello, " * name)
+
+# Vectors
+nums = [1, 2, 3, 4, 5]
+println(sum(nums))
+
+# Loop
+for i in 1:5
+    println(i * 2)
+end
+
+# Function
+function square(x)
+    return x^2
+end
+println(square(7))
